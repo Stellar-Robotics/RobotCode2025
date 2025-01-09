9f7b6f9b71d6d89f91e7484079fa7a457d8988d7
Gabe Melvin <gabemelvin14@gmail.com>
2025-01-09 18:40:30 -0500

    Created Intake and Elevator subsystems! Yay!

---
+++ src/main/java/frc/robot/RobotMechansims/PrimaryElevator/Subsystems/PrimaryElevator.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.RobotMechansims.PrimaryElevator.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PrimaryElevator extends SubsystemBase {
+  /** Creates a new PrimaryElevator. */
+  public PrimaryElevator() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
