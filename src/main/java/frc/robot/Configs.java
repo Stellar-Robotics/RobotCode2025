28796c4008163880f98896e43e6238f25eafa438
Cephas Lewis <lewis.cephasstephen@gmail.com>
2025-04-04 09:55:27 -0400

    changed current limit on the roller motor

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -104,7 +104,7 @@ public final class Configs {
 
             rollerMotorConfig
                 .idleMode(IdleMode.kBrake)
-                .smartCurrentLimit(30);
+                .smartCurrentLimit(40);
         
             rollerMotorConfig.closedLoop
                 .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
